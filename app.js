1eeb6efc63296dbb9e5129c3e2e0f92944af2c98
pawel26 <pjablonski10@gmail.com>
2016-02-17 13:37:35 +0000

    app.js update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var sqs=new AWS.SQS();
 //obiekt do obsługi simple DB z aws-sdk
 var simpledb = new AWS.SimpleDB();
 //GraphicsMagic
-var gm = require('gm');
+var gm = require('gm').subClass({imageMagick: true});
 
 //funkcja - petla wykonuje sie caly czas
 var myServer = function(){
@@ -51,32 +51,31 @@ var myServer = function(){
 			//handler do ussunięcia wiadomości z kolejki
 			var ReceiptHandle_forDelete = data.Messages[0].ReceiptHandle;
 			//{bucket, key}
-			console.log(data);
-			var messageinfo = JSON.parse(data.Messages[0].Body);
-			console.log("Otrzymano wiadomosc: bucket - "+messageinfo.bucket+", key - "+messageinfo.key);
+			console.log(data.Messages[0].Body);
+			var messageinfo = data.Messages[0].Body.split('/');
+			console.log("Otrzymano wiadomosc: bucket - "+messageinfo[0]+", key - "+messageinfo[1]);
 			
 			//to samo co wyzej tylko pobiera dane z metadanych a nie z body
-			//var messageinfo = { "bucket":data.Messages[0].MessageAttributes.bucket.StringValue,"key":data.Messages[0].MessageAttributes.key.StringValue}console.log(messageinfo.bucket);
+			//var messageinfo = { "bucket":messages[0],"key":data.Messages[0].MessageAttributes.key.StringValue}console.log(messageinfo.bucket);
 				
 			//parametry do pobrania pliku (obiektu)
 			var params2 = {
-				Bucket: messageinfo.bucket,
-				Key: messageinfo.key
+				Bucket: 'lab4-weeia',
+				//Prefix: 'pawel.jablonski/',
+				Key: messageinfo[0]+'/'+messageinfo[1],
+				//Region: "us-west-2"
+				
 			};
 			//zapisujemy plik z s3 na dysku
-			var file = require('fs').createWriteStream('tmp/'+messageinfo.key.substring(10));
+			var file = require('fs').createWriteStream('tmp/'+messageinfo[1]);
 			var requestt = s3.getObject(params2).createReadStream().pipe(file);
+                        //console.log(requestt);
 			//po zapisie na dysk
 			requestt.on('finish', function (){
 				console.log('jestem tu po zapisaniu pliku na dysk');
 
-				//zmieniamy coś w pliku roździelczość i cośtam jeszcze
-				gm('tmp/'+messageinfo.key.substring(10))
-				.implode(-1.2)
-				.contrast(-6)
-				//.resize(353, 257)
-				.autoOrient()
-				.write('tmp/'+messageinfo.key.substring(10), function (err) {
+				gm('tmp/'+messageinfo[1]).colors(40)
+				.write('tmp/'+messageinfo[1], function (err) {
 				if (err) {
 					console.log(err);
 				}
@@ -85,11 +84,12 @@ var myServer = function(){
 					console.log(' udalosie przetworzuc plik');	
 					
 					//wrzucamy na s3 nowy plik
-					var fileStream = require('fs').createReadStream('tmp/'+messageinfo.key.substring(10));
+					var fileStream = require('fs').createReadStream('tmp/'+messageinfo[1]);
 					fileStream.on('open', function () {
 						var paramsu = {
-							Bucket: messageinfo.bucket,
-							Key: 'processed/'+messageinfo.key.substring(10),
+							Bucket: 'lab4-weeia',
+							//Prefix: 'pawel.jablonski',
+							Key: 'processed/'+messageinfo[1],
 							ACL: 'public-read',
 							Body: fileStream,
 						};
@@ -106,12 +106,12 @@ var myServer = function(){
 							var paramsdb = {
 								Attributes: [
 									{ 
-									Name: messageinfo.key, 
+									Name: messageinfo[1], 
 									Value: "yes", 
 									Replace: true
 									}
 								],
-								DomainName: "czubakProjState", 
+								DomainName: "PawelKrzysiek", 
 								ItemName: 'ITEM001'
 							};
 							simpledb.putAttributes(paramsdb, function(err, datass) {
